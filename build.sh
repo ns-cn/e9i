d58a726b09630bc907ed8056e6a1e3a0092219cd
ns-cn <ns-cn@qq.com>
2023-03-26 18:53:51 +0800

    新增其他平台构建

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
 #!/bin/zsh
 
 fyne package -os darwin -appID com.tangyujun.e9i -name "E9I" .
+fyne package -os linux -appID com.tangyujun.e9i -name "E9I" .
+fyne package -os windows -appID com.tangyujun.e9i -name "E9I" .
